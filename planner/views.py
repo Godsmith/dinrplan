b8d6bcee2577bc22d79616d0ddd93ac280c98c03
Filip Lange <filip.lange@gmail.com>
2021-11-06 19:28:06 +0100

    Initial editday page

--- planner/views.py
+++ planner/views.py
@@ -1,5 +1,11 @@
+from datetime import date, timedelta
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "planner/index.html")
+    dates = [(date.today() + timedelta(days=i)).isoformat() for i in range(7)]
+    return render(request, "planner/index.html", {"dates": dates})
+
+
+def editday(request, date):
+    return render(request, "planner/editday.html", {"date": date})
