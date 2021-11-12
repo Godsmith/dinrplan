4d36fb0ac4114ab85015d3529fe6fc2eea9c0b90
Filip Lange <filip.lange@gmail.com>
2021-11-12 10:41:41 +0100

    Change to PostgreSQL

--- dinrplan/settings.py
+++ dinrplan/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "dinrplan.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dinrplan',
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
