5c36c4601776336963f1751a56c2a7f4573b1549
Filip Lange <filip.lange@gmail.com>
2021-11-16 17:14:42 +0100

    Add first css file to change link color of uncreated recipes

--- dinrplan/settings.py
+++ dinrplan/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Simplified static file serving
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "dinrplan.urls"
@@ -119,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
