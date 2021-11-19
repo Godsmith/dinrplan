58a14116fca8105c866175459ca93f85c1abb2b5
Filip Lange <filip.lange@gmail.com>
2021-11-19 19:17:07 +0100

    Add Category to admin page

--- planner/admin.py
+++ planner/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Day
 from .models import Meal
+from .models import Category
 
 admin.site.register(Day)
 admin.site.register(Meal)
+admin.site.register(Category)
