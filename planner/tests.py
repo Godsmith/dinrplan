87e0a00601a4a6927d743446a31797b2f085eb80
Filip Lange <filip.lange@gmail.com>
2021-11-21 22:46:37 +0100

    Add test for upload functionality

--- planner/tests.py
+++ planner/tests.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from pathlib import Path
 
 from django.test import TestCase
 
@@ -127,3 +128,21 @@ class ShowMealTests(TestCase):
         for text in ["user1", "My comment text"]:
             with self.subTest():
                 self.assertIn(text, str(response.content))
+
+
+class UploadTests(TestCase):
+    def setUp(self):
+        user1 = User.objects.create_user(username="user1", password="user1")
+        self.client.login(username="user1", password="user1")
+
+    def test_upload_creates_meal_and_day(self):
+        fixtures = Path(__file__).parent / "tests" / "fixtures"
+        with open(fixtures / "meals.json", "rb") as meals_file:
+            with open(fixtures / "days.json", "rb") as days_file:
+                self.client.post(
+                    reverse("planner:upload"),
+                    data={"meals": meals_file, "days": days_file},
+                )
+
+        self.assertEqual(Meal.objects.all()[0].name, "Vegobiffar med kryddsmör")
+        self.assertEqual(Day.objects.all()[0].date.isoformat(), "2020-02-18")
