c750ce9057b7a361a9fa201c53c6b10ddd1bd570
Filip Lange <filip.lange@gmail.com>
2021-11-26 21:01:22 +0100

    Fix no meal modal not opening on Recipes page

--- planner/tests/test_recipes.py
+++ planner/tests/test_recipes.py
@@ -17,6 +17,32 @@ def create_meal_for_today(day, user):
     day.meals.add(meal)
 
 
+def test_meal_modal_is_hidden_by_default(
+    logged_in_user_on_live_server, page, create_meal_for_today
+):
+    # Arrange
+    page.goto(logged_in_user_on_live_server.url + "/recipes")
+
+    # Act
+
+    # Assert
+    assert page.is_hidden(".modal")
+
+
+def test_clicking_recipe_opens_modal(
+    logged_in_user_on_live_server, page, create_meal_for_today
+):
+    # Arrange
+    page.goto(logged_in_user_on_live_server.url + "/recipes")
+
+    # Act
+    page.click(".exists")
+    page.wait_for_load_state("networkidle")
+
+    # Assert
+    assert page.is_visible(".modal")
+
+
 def test_not_created_recipes_are_not_visible_on_recipes_page(
     logged_in_user_on_live_server, page, day, user
 ):
